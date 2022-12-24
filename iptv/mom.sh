f0900b6aa8f83654460ffd58b4d11dff63bcadc0
Goodspeed <macbook-air@goodspeed.com>
2022-12-24 18:59:06 +0800

    20221224185906

--- iptv/mom.sh
+++ iptv/mom.sh
@@ -1,7 +1,5 @@
 filename=iptv/mom.m3u8
 echo '#EXTM3U' > $filename
 cat iptv/_includes/oversee.m3u >> $filename; echo -e '\n' >> $filename
-cat iptv/_includes/worldcup.m3u >> $filename; echo -e '\n' >> $filename
-cat iptv/_includes/cctv.m3u >> $filename; echo -e '\n' >> $filename
 cat iptv/_includes/provinces.stable.m3u >> $filename; echo -e '\n' >> $filename
-cat iptv/_includes/besttv.m3u >> $filename; echo -e '\n' >> $filename
+
