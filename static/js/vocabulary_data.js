6b8c6ff10b0690a90158cd5ffde84f6f873d37b1
Goodspeed <wangsong13@meituan.com>
2023-02-15 21:31:53 +0800

    20230215213153

---
+++ static/js/vocabulary_data.js
@@ -0,0 +1,60 @@
+const DATA = [
+    {
+        explanation: "to praise something or someone very much",
+        test: [
+            {
+                word: "extol",
+                sentence: "Though they extol the virtues of the peaceful life, only one of them has ever gone to live in the country and he was back in town within six months. Even he still lives under the illusion that country life is somehow superior to town life."
+            }]
+    },
+    {
+        explanation: "a good moral quality in a person, or the general quality of being morally good",
+        test: [
+            {
+                word: "virtues",
+                sentence: "Though they extol the virtues of the peaceful life, only one of them has ever gone to live in the country and he was back in town within six months. Even he still lives under the illusion that country life is somehow superior to town life."
+            }]
+    },
+    {
+        explanation: "A place or experience is extremely pleasant, beautiful, or peaceful",
+        test: [{
+            word: "idyllic",
+            sentence: "This idyllic pastoral scene is only part of the picture."
+        }]
+    }, {
+        explanation: "to deal with something unpleasant or annoying, or to continue existing despite bad or difficult conditions",
+        test: [{
+            word: "tolerate",
+            sentence: "Why people are prepared to tolerate a four-hour journey each day for the dubious privilege of living in the country is beyond me."
+        }]
+    },
+    {
+        explanation: "unusual and exciting because of coming (or seeming to come) from far away, especially in a tropical country",
+        test: [{
+            word: "exotic",
+            sentence: "stagger home loaded with as many of the exotic items as they can carry."
+        }]
+    },
+    {
+        explanation: "in an unreasonably determined way, especially by acting in a particular way and not changing at all, despite what anyone else says",
+        test: [{
+            word: "obstinately",
+            sentence: "It has always been a mystery to me why city dwellers, who appreciate all these things, obstinately pretend that they would prefer to live in the country."
+        }]
+    },
+    {
+        explanation: "the situation of being alone, often by choice",
+        test: [{
+            word: "solitude",
+            sentence: "Perhaps it is the desire for solitude or the chance of making an unexpected discovery that lures people down to the depths of the earth."
+        }]
+    },
+]
+
+/*
+{
+explanation: "",
+test: [{word: "",
+sentence: ""
+}]},
+*/
