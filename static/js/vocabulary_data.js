5c9956b87ab0c1efb2dfb61c2902922e50eb8f57
Goodspeed <wangsong13@meituan.com>
2023-02-21 14:49:32 +0800

    20230221144932

--- static/js/vocabulary_data.js
+++ static/js/vocabulary_data.js
@@ -1,122 +1,122 @@
 const DATA = [
     {
         explanation: "to praise something or someone very much",
-        test: [
-            {
-                word: "extol",
-                sentence: "Though they extol the virtues of the peaceful life, only one of them has ever gone to live in the country and he was back in town within six months. Even he still lives under the illusion that country life is somehow superior to town life."
-            }]
+        test:
+        {
+            word: "extol",
+            sentence: "Though they extol the virtues of the peaceful life, only one of them has ever gone to live in the country and he was back in town within six months. Even he still lives under the illusion that country life is somehow superior to town life."
+        }
     },
     {
         explanation: "a good moral quality in a person, or the general quality of being morally good",
-        test: [
-            {
-                word: "virtues",
-                sentence: "Though they extol the virtues of the peaceful life, only one of them has ever gone to live in the country and he was back in town within six months. Even he still lives under the illusion that country life is somehow superior to town life."
-            }]
+        test:
+        {
+            word: "virtues",
+            sentence: "Though they extol the virtues of the peaceful life, only one of them has ever gone to live in the country and he was back in town within six months. Even he still lives under the illusion that country life is somehow superior to town life."
+        }
     },
     {
         explanation: "A place or experience is extremely pleasant, beautiful, or peaceful",
-        test: [{
+        test: {
             word: "idyllic",
             sentence: "This idyllic pastoral scene is only part of the picture."
-        }]
+        }
     }, {
         explanation: "to deal with something unpleasant or annoying, or to continue existing despite bad or difficult conditions",
-        test: [{
+        test: {
             word: "tolerate",
             sentence: "Why people are prepared to tolerate a four-hour journey each day for the dubious privilege of living in the country is beyond me."
-        }]
+        }
     },
     {
         explanation: "unusual and exciting because of coming (or seeming to come) from far away, especially in a tropical country",
-        test: [{
+        test: {
             word: "exotic",
             sentence: "stagger home loaded with as many of the exotic items as they can carry."
-        }]
+        }
     },
     {
         explanation: "in an unreasonably determined way, especially by acting in a particular way and not changing at all, despite what anyone else says",
-        test: [{
+        test: {
             word: "obstinately",
             sentence: "It has always been a mystery to me why city dwellers, who appreciate all these things, obstinately pretend that they would prefer to live in the country."
-        }]
+        }
     },
     {
         explanation: "the situation of being alone, often by choice",
-        test: [{
+        test: {
             word: "solitude",
             sentence: "Perhaps it is the desire for solitude or the chance of making an unexpected discovery that lures people down to the depths of the earth."
-        }]
+        }
     },
     {
         explanation: "to allow yourself or another person to have something enjoyable, especially more than is good for you",
-        test: [{
+        test: {
             word: "indulge",
             sentence: "Inviting the fire brigade to put out a nonexistent fire is a crude form of deception which no self-respecting student would ever indulge in"
-        }]
+        }
     },
 
     {
         explanation: "in a frightening, violent, or powerful way",
-        test: [{
+        test: {
             word: "fiercely",
             sentence: "the workmen struggled fiercely."
-        }]
+        }
     },
 
     {
         explanation: "to take using sudden force",
-        test: [{
+        test: {
             word: "seize",
             sentence: "the police attempted to seize the pneumatic drill."
-        }]
+        }
     },
     {
 
         explanation: "a very large rock",
-        test: [{
+        test: {
             word: "boulders",
             sentence: "the road was littered with boulders."
-        }]
+        }
     }, {
         explanation: "to change direction, especially suddenly",
-        test: [{
+        test: {
             word: "swerved",
             sentence: "we swerved to avoid large boulders."
-        }]
+        }
     }, {
         explanation: "in a way that suggests that something unpleasant is likely to happen",
-        test: [{
+        test: {
             word: "ominously",
             sentence: "The wheels scooped up stones that hammered ominously under the car."
-        }]
+        }
     }, {
         explanation: "to move something with a scoop or with something used as a scoop",
-        test: [{
+        test: {
             word: "scooped",
             sentence: "The wheels scooped up stones that hammered ominously under the car."
-        }]
+        }
     }, {
         explanation: "a line or pattern that looks like a Z or a row of Zs joined together",
-        test: [{
+        test: {
             word: "zigzag",
             sentence: "He followed its zigzag course."
-        }]
+        }
     },
     {
         explanation: "to look at or consider a person or thing carefully and in detail in order to discover something about them",
-        test: [{
+        test: {
             word: "examine",
             sentence: "We saw Bruce remaining in the car when we all got out to examine the fissure."
-        }]
+        }
     },
 ]
 
 /*
 {
 explanation: "",
-test: [{word: "",
-sentence: ""
-}]},
+test: {word: "",
+sentence: ""}
+},
 */
