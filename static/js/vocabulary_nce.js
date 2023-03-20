fe04c9400b1ed807a58a30ae51255525b7fd30f6
Goodspeed <macbook-air@goodspeed.com>
2023-03-20 19:32:09 +0800

    20230320193209

--- static/js/vocabulary_nce.js
+++ static/js/vocabulary_nce.js
@@ -1,4 +1,10 @@
 const DATA_nce2 = [
+    { explanation: "especially", word: "特别是", },
+    { explanation: "teenagers", word: "青少年", },
+    { explanation: "influence", word: "影响", },
+    { explanation: "justice", word: "正义", },
+    { explanation: "burglar", word: "窃贼", },
+    { explanation: "series", word: "系列", },
 ];
 /*
 please translate the following words to Chinese. 
