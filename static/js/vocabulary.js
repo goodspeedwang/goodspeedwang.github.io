6bc126d2087baa73aef31e62cfce5e1d24778529
Goodspeed <wangsong13@meituan.com>
2023-04-04 11:36:44 +0800

    202304 4113644

--- static/js/vocabulary.js
+++ static/js/vocabulary.js
@@ -91,7 +91,9 @@ function tips() {
 }
 
 class storage {
+    //https://keyvalue.immanuel.co/
     constructor(key) {
+        this.APPKEY = "imrpbe06";
         this.key = key + "";
         this.number = 0;
         this.updateTime = Date.now();
@@ -106,16 +108,57 @@ class storage {
             this.number = obj.number;
             this.updateTime = updateTime;
         }
+
+        const url = `https://keyvalue.immanuel.co/api/KeyVal/GetValue/${this.APPKEY}/${this.key}`;
+        fetch(url).then(response => response.json())
+            .then(data => this.number = parseInt(data))
+            .catch();
     }
     increase() {
-        var object = {
+        let object = {
             updateTime: Date.now(),
             number: this.number + 1
         }
         localStorage.setItem(this.key, JSON.stringify(object));
+
+        let url = `https://keyvalue.immanuel.co/api/KeyVal/GetValue/${this.APPKEY}/${this.key}`
+        this.get(url).then(response => {
+            let number = 0;
+            let object = null;
+            try {
+                let object = JSON.parse(response)
+                this.number = object.number;
+                this.updateTime = object.updateTime;
+            } catch (error) {
+
+            }
+            object = {
+                updateTime: Date.now(),
+                number: this.number + 1
+            }
+            url = `https://keyvalue.immanuel.co/api/KeyVal/UpdateValue/${this.APPKEY}/${this.key}/${JSON.stringify(object)}`;
+            console.log(url);
+            this.get(url, "POST").then(response => console.log(response)).catch(error => console.error(error));
+        });
     }
     del() {
         localStorage.removeItem(this.key);
+        var object = {
+            updateTime: Date.now(),
+            number: 0
+        }
+        const url = `https://keyvalue.immanuel.co/api/KeyVal/UpdateValue/${this.APPKEY}/${this.key}/${JSON.stringify(object)}`;
+        this.get(url, "POST");
+    }
+
+    get(url, method = "GET") {
+        return new Promise((resolve, reject) => {
+            const xhr = new XMLHttpRequest();
+            xhr.open(method, url);
+            xhr.onload = () => resolve(xhr.responseText);
+            xhr.onerror = () => reject(xhr.statusText);
+            xhr.send();
+        });
     }
 }
 
