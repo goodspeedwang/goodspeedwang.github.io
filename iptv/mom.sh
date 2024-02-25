c4e79dca0cb733fa98c97b60b5c7aeea72629ba5
Goodspeed <macbook-air@goodspeed.com>
2024-02-25 18:38:03 +0800

    20240225183803

--- iptv/mom.sh
+++ iptv/mom.sh
@@ -3,4 +3,5 @@ echo '#EXTM3U' > $filename
 cat iptv/_includes/oversee.stable.m3u >> $filename; echo -e '\n' >> $filename
 #cat iptv/_includes/cctv.stable.m3u >> $filename; echo -e '\n' >> $filename
 cat iptv/_includes/provinces.stable.m3u >> $filename; echo -e '\n' >> $filename
+cat iptv/_includes/movie.m3u >> $filename; echo -e '\n' >> $filename
 
