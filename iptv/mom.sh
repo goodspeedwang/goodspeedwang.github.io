a3d8c144f61d32f10eefe5c26d752b272d94d19f
Macbook M1 Pro <goodspeedwang@gmail.com>
2026-08-21 11:08:31 +0800

    20260821110831

--- iptv/mom.sh
+++
@@ -1,7 +0,0 @@
-filename=iptv/mom.m3u8
-echo '#EXTM3U' > $filename
-cat iptv/_includes/oversee.stable.m3u >> $filename; echo -e '\n' >> $filename
-#cat iptv/_includes/cctv.stable.m3u >> $filename; echo -e '\n' >> $filename
-cat iptv/_includes/provinces.stable.m3u >> $filename; echo -e '\n' >> $filename
-cat iptv/_includes/movie.m3u >> $filename; echo -e '\n' >> $filename
-
